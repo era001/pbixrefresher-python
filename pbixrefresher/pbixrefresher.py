b5029b22cf74b63f0a47ecda12336204d5406ed9
era001 <85181618+era001@users.noreply.github.com>
2021-12-02 13:11:02 -0500

    sample working with cloud

--- pbixrefresher/pbixrefresher.py
+++ pbixrefresher/pbixrefresher.py
@@ -13,20 +13,24 @@ def type_keys(string, element):
         element.type_keys(char)
 
 def main():   
-	# Parse arguments from cmd
-	parser = argparse.ArgumentParser()
-	parser.add_argument("workbook", help = "Path to .pbix file")
-	parser.add_argument("--workspace", help = "name of online Power BI service work space to publish in", default = "My workspace")
-	parser.add_argument("--refresh-timeout", help = "refresh timeout", default = 30000, type = int)
-	parser.add_argument("--no-publish", dest='publish', help="don't publish, just save", default = True, action = 'store_false' )
-	parser.add_argument("--init-wait", help = "initial wait time on startup", default = 15, type = int)
-	args = parser.parse_args()
+##	# Parse arguments from cmd
+##	parser = argparse.ArgumentParser()
+##	parser.add_argument("workbook", help = "Path to .pbix file")
+##	parser.add_argument("--workspace", help = "name of online Power BI service work space to publish in", default = "My workspace")
+##	parser.add_argument("--refresh-timeout", help = "refresh timeout", default = 30000, type = int)
+##	parser.add_argument("--no-publish", dest='publish', help="don't publish, just save", default = True, action = 'store_false' )
+##	parser.add_argument("--init-wait", help = "initial wait time on startup", default = 15, type = int)
+##	args = parser.parse_args()
 
 	timings.after_clickinput_wait = 1
-	WORKBOOK = args.workbook
-	WORKSPACE = args.workspace
-	INIT_WAIT = args.init_wait
-	REFRESH_TIMEOUT = args.refresh_timeout
+	#WORKBOOK = args.workbook
+	WORKBOOK = r"C:\Git\pbixrefresher-python\sample.pbix"
+	#WORKSPACE = args.workspace
+	WORKSPACE = "My workspace"
+	#INIT_WAIT = args.init_wait
+	INIT_WAIT = 15
+	#REFRESH_TIMEOUT = args.refresh_timeout
+	REFRESH_TIMEOUT = 30000
 
 	# Kill running PBI
 	PROCNAME = "PBIDesktop.exe"
@@ -42,17 +46,24 @@ def main():
 	print("Waiting ",INIT_WAIT,"sec")
 	time.sleep(INIT_WAIT)
 
-	# Connect pywinauto
+##	# Connect pywinauto
+##	print("Identifying Power BI window")
+##	app = Application(backend = 'uia').connect(path = PROCNAME)
+##	win = app.window(title_re = '.*Power BI Desktop')
+##	time.sleep(5)
+##	win.wait("enabled", timeout = 300)
+##	win.Save.wait("enabled", timeout = 300)
+##	win.set_focus()
+##	win.Home.click_input()
+##	win.Save.wait("enabled", timeout = 300)
+##	win.wait("enabled", timeout = 300)
+
+	# Connect pywinauto - new from https://github.com/DarknessTech/pbixrefresher2020/blob/master/pbixrefresher.py
 	print("Identifying Power BI window")
 	app = Application(backend = 'uia').connect(path = PROCNAME)
 	win = app.window(title_re = '.*Power BI Desktop')
 	time.sleep(5)
-	win.wait("enabled", timeout = 300)
-	win.Save.wait("enabled", timeout = 300)
 	win.set_focus()
-	win.Home.click_input()
-	win.Save.wait("enabled", timeout = 300)
-	win.wait("enabled", timeout = 300)
 
 	# Refresh
 	print("Refreshing")
@@ -64,26 +75,45 @@ def main():
 
 	# Save
 	print("Saving")
-	type_keys("%1", win)
+	#type_keys("%1", win)
+	#type_keys("^S", win)
+	win.Save.wait("enabled", timeout = 300)
 	#wait_win_ready(win)
 	time.sleep(5)
 	win.wait("enabled", timeout = REFRESH_TIMEOUT)
 
 	# Publish
-	if args.publish:
-		print("Publish")
+##	if args.publish:
+	if True == True:
+##		print("Publish")
+##		win.Publish.click_input()
+##		publish_dialog = win.child_window(auto_id = "KoPublishToGroupDialog")
+##		publish_dialog.child_window(title = WORKSPACE).click_input()
+##		publish_dialog.Select.click()
+##		try:
+##			win.Replace.wait('visible', timeout = 10)
+##		except Exception:
+##			pass
+##		if win.Replace.exists():
+##			win.Replace.click_input()
+##		win["Got it"].wait('visible', timeout = REFRESH_TIMEOUT)
+##		win["Got it"].click_input()
+            
+                # new from https://github.com/DarknessTech/pbixrefresher2020/blob/master/pbixrefresher.py
+		print("Save and Publish")
 		win.Publish.click_input()
+		time.sleep(10)
+		save_dialog = win.child_window(auto_id = "modalDialog")
+		save_dialog.Save.click_input()
+		time.sleep(10)
 		publish_dialog = win.child_window(auto_id = "KoPublishToGroupDialog")
-		publish_dialog.child_window(title = WORKSPACE).click_input()
-		publish_dialog.Select.click()
-		try:
-			win.Replace.wait('visible', timeout = 10)
-		except Exception:
-			pass
-		if win.Replace.exists():
-			win.Replace.click_input()
-		win["Got it"].wait('visible', timeout = REFRESH_TIMEOUT)
-		win["Got it"].click_input()
+		publish_dialog.child_window(title = WORKSPACE, found_index=1).click_input()
+		time.sleep(10)
+		publish_dialog.Select.click_input()
+		time.sleep(10)
+		replace_dialog = win.child_window(auto_id = "KoPublishWithImpactViewDialog")
+		replace_dialog.Replace.click_input()
+		time.sleep(300)
 
 	#Close
 	print("Exiting")
